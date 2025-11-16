49cd23f317f8d4c680e0a56adb836f8f3f89c9b5
alexandrejs13 <144365406+alexandrejs13@users.noreply.github.com>
2025-11-15 21:42:23 -0300

    Add files via upload

--- pages/7_Dashboard.py
+++ pages/7_Dashboard.py
@@ -1,11 +1,16 @@
 import streamlit as st
 
-# Cabeçalho com ícone (nativo)
-col1, col2 = st.columns([0.15, 0.85])
+# LOGO EM TODAS AS PÁGINAS
+with st.sidebar:
+    st.image("assets/icons/SIG_Logo_RGB_Black.png", width=140)
+    st.markdown("---")
+
+# Cabeçalho com ícone nítido e alinhado
+col1, col2 = st.columns([0.10, 0.90])
 with col1:
-    st.image("assets/icons/data_2_perfromance.png", width=36)
+    st.image("assets/icons/data_2_perfromance.png", width=28)
 with col2:
-    st.header("Dashboard")
+    st.markdown("## Dashboard")
 
 st.markdown("---")
 st.write("Conteúdo da página Dashboard.")
